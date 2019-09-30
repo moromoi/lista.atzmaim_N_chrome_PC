f76ff90ea6ec70c105950f4bbca7d6cca3876a61
moromoi <alex.palchisky@gmail.com>
2019-09-30 21:00:51 +0300

    Create SingUP tests

--- src/main/java/Singup.java
+++
@@ -1,7 +0,0 @@
-public class Singup {
-
-public static void main(String[] args) {
-System.out.println("Hellow");
-}
-
-}
---
+++ src/main/java/im/atzma/lista/Square.java
@@ -0,0 +1,15 @@
+package im.atzma.lista;
+
+public class Square {
+
+    int s;
+
+    public Square(int S) {
+        this.s = S;
+    }
+
+
+    public int area() {
+        return this.s * this.s;
+    }
+}
