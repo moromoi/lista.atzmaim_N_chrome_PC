2bf76e1071154bab62f32cc8c8871107ce173f20
moromoi <alex.palchisky@gmail.com>
2020-01-10 00:07:45 +0200

    fix

--- src/test/java/im/atzma/lista/tests/TestBase.java
+++ src/test/java/im/atzma/lista/tests/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
 
     }
 
-//    @AfterSuite(alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
